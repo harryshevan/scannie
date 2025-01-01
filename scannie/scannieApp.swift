0f93f2b2c74c6e289a5c4fd82983fb5569338565
Igor Shevchenko <baplok@yandex.ru>
2025-01-01 22:04:59 +0300

    Initial Commit

---
+++ scannie/scannieApp.swift
@@ -0,0 +1,17 @@
+//
+//  scannieApp.swift
+//  scannie
+//
+//  Created by harrysh on 01.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct scannieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
